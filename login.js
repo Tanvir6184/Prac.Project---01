efb0fc6f451657c3797b1cf90478aeff6108b988
Tanvir6184 <goldenittanvirzain61@gmail.com>
2024-09-19 01:11:03 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,7 @@
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault(); 
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
