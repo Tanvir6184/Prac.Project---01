8f646f1a383dae62e527fc463a2b440377690ce4
Tanvir6184 <goldenittanvirzain61@gmail.com>
2024-09-19 02:26:22 +0600

    second commit

--- login.js
+++ login.js
@@ -1,7 +1,21 @@
 
 document.getElementById('button-login').addEventListener('click', function(event){
+
     event.preventDefault(); 
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber)
+
+// temporary work do not do this 
+
+    if(phoneNumber === '1' && pinNumber === '1234'){
+        
+        window.location.href = '/home.html'
+       
+    }
+    else {
+        return alert('wrong pin or phone number')
+    }
 })
